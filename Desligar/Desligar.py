18dc34c712be547ef63797533bf55febe3ee459f
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-05-23 16:29:31 -0300

    Update Desligar.py

--- Desligar/Desligar.py
+++ Desligar/Desligar.py
@@ -2,7 +2,7 @@ import os
 from tkinter import *
 from tkinter import ttk
 
-def Desligar():
+"""def Desligar():
     desligar= input("Quer desligar sue pc?")
     if (desligar == "SIM" or "sim" or "S" or "s"):
         def desligar_computador():
@@ -15,5 +15,5 @@ root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Sim", command=Desligar()).grid(column=1, row=0)
+ttk.Button(frm, text="Sim", command=root.destroy).grid(column=1, row=0)
 root.mainloop()
