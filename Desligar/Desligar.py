1625e59e7767ec7477cf5061c49fdd803f4b575c
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-05-23 19:04:19 -0300

    Update Desligar.py

--- Desligar/Desligar.py
+++ Desligar/Desligar.py
@@ -2,17 +2,13 @@ import os
 from tkinter import *
 from tkinter import ttk
 
-
-if
 def Desligar():
     os.system("shutdown /s /t 1")
-
-   
 
 root = Tk()
 frm = ttk.Frame(root, padding=40)
 frm.grid()
 ttk.Label(frm, text="Desligar").grid(column=0, row=0)
-ttk.Button(frm, text="Sim", command=Desligar()).grid(column=0, row=2)
+ttk.Button(frm, text="Sim", command=Desligar).grid(column=0, row=2)
 ttk.Button(frm, text="Não", command=root.destroy).grid(column=0, row=4)
 root.mainloop()
