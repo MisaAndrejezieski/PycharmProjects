c681135f7af39c3bf27e9f9ef9990a5802bedced
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-05-23 15:46:30 -0300

    Update Desligar.py

--- Desligar/Desligar.py
+++ Desligar/Desligar.py
@@ -4,15 +4,17 @@ from tkinter import *
 desligar= input("Quer desligar sue pc?")
 
 
-if (desligar == "SIM" or "sim" or "S" or "s"):
+/*if (desligar == "SIM" or "sim" or "S" or "s"):
     def desligar_computador():
         os.system("shutdown /s /t 1")
         desligar_computador()
 else:
-    print("Desistiu???")
+    print("Desistiu???")*/
 
 janela = Tk()
 janela.title("Desligando o computador...")
 
+texto_orientacao = Label(janela, text="Desligar")
+texto_orientacao.grid(column=0, row=0)
 
 janela.mainloop()
