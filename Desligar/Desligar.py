116b6a8f7b74e67ab174a6f323fa94dbc87c4ce1
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-05-25 22:39:08 -0300

    Update Desligar.py

--- Desligar/Desligar.py
+++ Desligar/Desligar.py
@@ -1,14 +1,16 @@
-import os
-from tkinter import *
-from tkinter import ttk
-misa
-def Desligar():
-    os.system("shutdown /s /t 1")
+import os  # Importa o módulo os que permite a interação com o sistema operacional
+from tkinter import *  # Importa tudo da biblioteca tkinter para criar interfaces gráficas
+from tkinter import ttk  # Importa ttk da biblioteca tkinter para usar widgets temáticos
 
-root = Tk()
-frm = ttk.Frame(root, padding=40)
-frm.grid()
-ttk.Label(frm, text="Desligar").grid(column=0, row=0)
-ttk.Button(frm, text="Sim", command=Desligar).grid(column=0, row=2)
-ttk.Button(frm, text="Não", command=root.destroy).grid(column=0, row=4)
-root.mainloop()
+# 
+
+def Desligar():  # Define uma função chamada 'Desligar'
+    os.system("shutdown /s /t 1")  # Executa o comando de desligamento do sistema operacional
+
+root = Tk()  # Cria a janela principal
+frm = ttk.Frame(root, padding=40)  # Cria um frame com padding de 40
+frm.grid()  # Posiciona o frame na janela
+ttk.Label(frm, text="Desligar").grid(column=0, row=0)  # Cria um rótulo 'Desligar' no frame
+ttk.Button(frm, text="Sim", command=Desligar).grid(column=0, row=2)  # Cria um botão 'Sim' que, quando pressionado, executa a função 'Desligar'
+ttk.Button(frm, text="Não", command=root.destroy).grid(column=0, row=4)  # Cria um botão 'Não' que, quando pressionado, fecha a janela
+root.mainloop()  # Inicia o loop principal da janela
